4e138b786e5802f3957e62a848ba59f3a12dabed
dragana5 <dstanivukovic@hotmail.com>
2017-07-05 17:23:33 +0100

    clearer version of js

--- js/app.js
+++ js/app.js
@@ -122,124 +122,258 @@
 // }, 3000);
 
 // always run function(init) at the begginging of your code in order for the dom to be run
+// $(init);
+//
+// // all vars stated in global scope
+//
+// // a var with all the images stored in there
+// const imagesArray   = ['image1.jpg','image2.jpg','image3.jpg','image4.jpg','image5.jpg','image6.jpg','image7.jpg','image8.jpg','image9.jpg','image10.jpg','image1.jpg','image12.jpg'];
+// // counter var declared in global scope set to 0 only to be increased when running showImages()
+// let counter         = 0;
+// // an empty var to store the randomImages selected in pickRandomImages()
+// const randomImages  = [];
+// // ???????????????? check this let score!!!!! created to store scores as the game is played
+// let $score = 0;
+// // ??????????? check this let!!!!!!!!!!!! created to store number of attempts as the game is played
+// let $attempts = 0;
+// // vars for each dom element declared in global scope with a value of null in order to be accessed outside the init()
+// let $startGame      = null;
+// let $buttonYes      = null;
+// let $buttonNo       = null;
+// let $imageContainer = null;
+// // function to load all the dom elements and they are all converted to jQuery objects
+// function init() {
+//   $startGame       = $('#startGame');
+//   $buttonYes       = $('#yesButton');
+//   $buttonNo        = $('#noButton');
+//   $imageContainer  = $('.images');
+//   // load attempts and score elements, maybe instructions??????
+//   $score           = $('.score');
+//   $attempts        = $('.attempts');
+//
+//   // inside init() call pickRandomImages()
+//   pickRandomImages();
+//   // pickRandomImages() declared
+//   function pickRandomImages() {
+//     // making sure that pickRandomImages() loops through imagesArray only twice to pick only 2 images at the time
+//     for (let i = 0; i < 2; i++) {
+//       // pick a random image
+//       const randomImage = imagesArray[Math.floor(Math.random()*imagesArray.length)];
+//       // push a randomImage selected to a randomImage array in global scope
+//       randomImages.push(randomImage);
+//     }
+//
+//
+//     // after both images are selected call showImages() to display them
+//     showImages();
+//
+//   }
+//   // showImages() declared
+//   function showImages() {
+//     // the div where the images will be shown changes removes the class .hidden beceause it needs to be shown
+//     $($imageContainer).removeClass('hidden');
+//
+//     // store the setInterval() because it has to be called to clear the end of showImages()
+//     const imagesInterval = setInterval(function() {
+//       // change the background image to the image that was selected and stored in randomImages array with an index value of counter value
+//       $($imageContainer).css('background-image', `url(./images/${randomImages[counter]})`);
+//       // timeout() is a part of showImages() to be able to take away the shown image
+//       setTimeout(function() {
+//         // change the background-image of imageContainer to none to make the image dissapear
+//         $($imageContainer).css('background-image', `none`);
+//         // set the time for the change of the background to none to 2 sec which will make the image stay in the div for 2 sec
+//       }, 2000);
+//       // increase the counter by 1 to move onto the second image or clear interval if the counter is 2
+//       counter++;
+//
+//       if (counter === 2) {
+//         clearInterval(imagesInterval);
+//         // set the counter to 0 after clearing interval in order to start again
+//         counter = 0;
+//         // call showButtons() after the second image has been displayed
+//         showButtons();
+//       }
+//       // display the second image for 3 sec
+//     }, 3000);
+//   }
+//
+//   function showButtons() {
+//     console.log('show buttons');
+//
+//     // show buttons
+//     $buttonYes.css('display', `block`);
+//     $buttonNo.css('display', `block`);
+//
+//     addEventListeners();
+//   }
+//
+//
+//
+//   function addEventListeners() {
+//     // add event listeners on buttons
+//     // $buttonYes.addEventListener('click', randomImagesComparisonYes);
+//     $buttonYes.on('click', randomImagesComparisonYes);
+//     // $buttonNo.addEventListener('click', randomImagesComparisonNo;
+//     $buttonNo.on('click', randomImagesComparisonNo);
+//   }
+//
+//
+//   // dependng on choice 'yes' or 'no'
+//
+//
+//
+//   // compare the two elements in the array
+//
+//   function randomImagesComparisonYes() {
+//     if(randomImages[0] === randomImages[1]) {
+//       console.log('works');
+//       // increase the score value by 1
+//       $score += 1;
+//     } else {
+//       //    increase the attempts value by 1
+//       $attempts += 1;
+//     }
+//   }
+//
+//   function randomImagesComparisonNo() {
+//     if( randomImages[0] !== randomImages[1]) {
+//       $score += 1;
+//     } else {
+//       $attempts += 1;
+//     }
+//   }
+//
+// }
+//
+// // method to display the score as it is being updated
+//
+// $('.score').text('You got ' + $score + 'right in ' + $attempts +'.');
+//
+// // level up, make the interval/timeout shorter
+//
+//
+//
+// // reset function
+//
+//
+// //
+//
+//
+// // to add text to the div use
+// // $('selected element').text("Welcome");
+
 $(init);
 
-// all vars stated in global scope
-
-// a var with all the images stored in there
-const imagesArray   = ['image1.jpg','image2.jpg','image3.jpg','image4.jpg','image5.jpg','image6.jpg','image7.jpg','image8.jpg','image9.jpg','image10.jpg','image1.jpg','image12.jpg'];
-// counter var declared in global scope set to 0 only to be increased when running showImages()
-let counter         = 0;
-// an empty var to store the randomImages selected in pickRandomImages()
 const randomImages  = [];
-// ???????????????? check this let score!!!!! created to store scores as the game is played
-let score = null;
-// ??????????? check this let!!!!!!!!!!!! created to store number of attempts as the game is played
-let attempts = null;
-// vars for each dom element declared in global scope with a value of null in order to be accessed outside the init()
+const imagesArray   = ['image1.jpg','image2.jpg','image3.jpg','image4.jpg','image5.jpg','image6.jpg','image7.jpg','image8.jpg','image9.jpg','image10.jpg','image1.jpg','image12.jpg'];
+let counter         = 0;
+let $score          = 0;
+let $attempts       = 0;
 let $startGame      = null;
 let $buttonYes      = null;
 let $buttonNo       = null;
 let $imageContainer = null;
-// function to load all the dom elements and they are all converted to jQuery objects
+
 function init() {
   $startGame       = $('#startGame');
   $buttonYes       = $('#yesButton');
   $buttonNo        = $('#noButton');
   $imageContainer  = $('.images');
-  // load attempts and score elements, maybe instructions??????
-  const $score           = $('.score');
-  const $attempts        = $('.attempts');
-  const $instructions    = $('.instructions');
-// inside init() call pickRandomImages()
+  $score           = $('.score');
+  $attempts        = $('.attempts');
+
   pickRandomImages();
-}
-// pickRandomImages() declared
-function pickRandomImages() {
-  // making sure that pickRandomImages() loops through imagesArray only twice to pick only 2 images at the time
-  for (let i = 0; i < 2; i++) {
-    // pick a random image
-    const randomImage = imagesArray[Math.floor(Math.random()*imagesArray.length)];
-    // push a randomImage selected to a randomImage array in global scope
-    randomImages.push(randomImage);
+  function pickRandomImages() {
+    for (let i = 0; i < 2; i++) {
+      const randomImage = imagesArray[Math.floor(Math.random()*imagesArray.length)];
+      randomImages.push(randomImage);
+    }
+
+    showImages();
+  }
+  function showImages() {
+    $($imageContainer).removeClass('hidden');
+    const imagesInterval = setInterval(function() {
+      $($imageContainer).css('background-image', `url(./images/${randomImages[counter]})`);
+      setTimeout(function() {
+        $($imageContainer).css('background-image', `none`);
+      }, 2000);
+      counter++;
+
+      if (counter === 2) {
+        clearInterval(imagesInterval);
+        counter = 0;
+        showButtons();
+      }
+    }, 3000);
   }
 
+  function showButtons() {
+    $buttonYes.css('display', `block`);
+    $buttonNo.css('display', `block`);
+    addEventListeners();
+  }
 
-// after both images are selected call showImages() to display them
-  showImages();
-
-}
-// showImages() declared
-function showImages() {
-  // the div where the images will be shown changes removes the class .hidden beceause it needs to be shown
-  $($imageContainer).removeClass('hidden');
-// store the setInterval() because it has to be called to clear the end of showImages()
-  const imagesInterval = setInterval(function() {
-    // change the background image to the image that was selected and stored in randomImages array with an index value of counter value
-    $($imageContainer).css('background-image', `url(./images/${randomImages[counter]})`);
-// timeout() is a part of showImages() to be able to take away the shown image
-    setTimeout(function() {
-      // change the background-image of imageContainer to none to make the image dissapear
-      $($imageContainer).css('background-image', `none`);
-      // set the time for the change of the background to none to 2 sec which will make the image stay in the div for 2 sec
-    }, 2000);
-    // increase the counter by 1 to move onto the second image or clear interval if the counter is 2
-    counter++;
-
-    if (counter === 2) {
-      clearInterval(imagesInterval);
-      // set the counter to 0 after clearing interval in order to start again
-      counter = 0;
-      // call showButtons() after the second image has been displayed
-      showButtons();
-    }
-    // display the second image for 3 sec
-  }, 3000);
-}
-
-function showButtons() {
-  console.log('show buttons');
-
-  // show buttons
-  $($buttonYes).css('display', `block`);
-  $($buttonNo).css('display', `block`);
-
-
-
-
-  // add event listeners on buttons
-  $buttonYes.addEventListener('click', randomImagesComparisonYes());
-  $buttonNo.addEventListener('click', randomImagesComparisonNo());
-
-
-  // dependng on choice 'yes' or 'no'
-
-
-
-  // compare the two elements in the array
+  function addEventListeners() {
+    $buttonYes.on('click', randomImagesComparisonYes);
+    $buttonNo.on('click', randomImagesComparisonNo);
+  }
 
   function randomImagesComparisonYes() {
+    $attempts += 1;
+
     if(randomImages[0] === randomImages[1]) {
-  // increase the score value by 1
-      score += 1;
-    } else {
-  //    increase the attempts value by 1
-      attempts += 1;
+      $score += 1;
     }
+
+    updateDisplay();
+    pickRandomImages();
   }
 
   function randomImagesComparisonNo() {
+    $attempts += 1;
     if( randomImages[0] !== randomImages[1]) {
-      score += 1;
-      console.log('it works');
-    } else {
-      attempts += 1;
+      $score += 1;
     }
+
+    updateDisplay();
+    pickRandomImages();
   }
 
+  function updateDisplay() {
+    $('.score').text('You got ' + $score + 'right in ' + $attempts +'.');
+  }
 }
 
+  function reset() {
+    const randomImages  = [];
+    let counter         = 0;
+    let $score          = 0;
+    let $attempts       = 0;
+}
+
+let timer = 60;
+
+setInterval(function(){
+  timer -= 1;
+
+  if (timer === 0) {
+    alert('You ran out of time!');
+    // prompt('Would you like to play again?')
+    // if (input = 'yes') {
+    //   $(init);
+    //   pickRandomImages();
+    //   if (not){
+    //     exit;
+    //   }
+    // } ;
+  }
+}, 1000);
 
 
-// to add text to the div use
-// $('selected element').text("Welcome");
+
+// this is an example for exiting the function
+// var result = prompt("OK?");
+// if (result === null) {
+//     return;
+// }
